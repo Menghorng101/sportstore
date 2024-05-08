bfed6c7d7976462efb8d3324257fd81d604c6489
Menghorng101 <menghorng414@gmail.com>
2024-05-09 00:16:25 +0700

    change

--- js/app.js
+++ js/app.js
@@ -1,5 +1,5 @@
 let menubar = document.querySelector("#menu-bar");
-let navbar =document.querySelector(".navbar");
+let navbar = document.querySelector(".navbar");
 
 //step 1:
 // menubar.addEventListener("click", function(){
@@ -8,65 +8,64 @@ let navbar =document.querySelector(".navbar");
 // });
 
 //step 2:
-menubar.onclick = () =>{
-    menubar.classList.toggle("fa-times");
-    navbar.classList.toggle("active");
-}
+menubar.onclick = () => {
+  menubar.classList.toggle("fa-times");
+  navbar.classList.toggle("active");
+};
 
-window.onscroll = () =>{
-    menubar.classList.remove('fa-times');
-    navbar.classList.remove('active');
-}
+window.onscroll = () => {
+  menubar.classList.remove("fa-times");
+  navbar.classList.remove("active");
+};
 
-document.querySelector('#search-icon').onclick = () => {
-    document.querySelector('#search-form').classList.toggle('active');
-}
+document.querySelector("#search-icon").onclick = () => {
+  document.querySelector("#search-form").classList.toggle("active");
+};
 
-document.querySelector('#close').onclick = () =>{
-    document.querySelector('#search-form').classList.remove('active');
-
-}
+document.querySelector("#close").onclick = () => {
+  document.querySelector("#search-form").classList.remove("active");
+};
 
 var swiper = new Swiper(".home-slider", {
-    spaceBetween: 30,
-    centeredSlides: true,
-    autoplay: {
-      delay: 5500,
-      disableOnInteraction: false,
-    },
-    pagination: {
-      el: ".swiper-pagination",
-      clickable: true,
-    },
-    loop: true,
+  spaceBetween: 30,
+  centeredSlides: true,
+  autoplay: {
+    delay: 5500,
+    disableOnInteraction: false,
+  },
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  loop: true,
 });
 
 //review
 var swiper = new Swiper(".review-slider", {
-    spaceBetween: 20,
-    centeredSlides: true,
-    autoplay: {
-      delay: 5500,
-      disableOnInteraction: false,
+  spaceBetween: 20,
+  centeredSlides: true,
+  autoplay: {
+    delay: 5500,
+    disableOnInteraction: false,
+  },
+  loop: true,
+  breakpoints: {
+    0: {
+      slidesPerView: 1,
     },
-    loop: true,
-    breakpoints: {
-      0: {
-        slidesPerView: 1,
-      },
 
-      640: {
-        slidesPerView: 2,
-      },
-
-      768: {
-        slidesPerView: 2,
-      },
-      
-      1024: {
-        slidesPerView: 3,
-      },
+    640: {
+      slidesPerView: 2,
     },
+
+    768: {
+      slidesPerView: 2,
+    },
+
+    1024: {
+      slidesPerView: 3,
+    },
+  },
 });
 
 //new-arrivals
@@ -90,11 +89,24 @@ var swiper = new Swiper(".arrivals-slider", {
     768: {
       slidesPerView: 3,
     },
-    
+
     1024: {
       slidesPerView: 4,
     },
   },
 });
 
-//
+//about
+document.querySelectorAll(".about-image-1").forEach((image_1) => {
+  image_1.addEventListener("click", () => {
+    var src = image_1.getAttribute("src");
+    document.querySelector('.big-image-1').src = src;
+  });
+});
+
+document.querySelectorAll(".about-image-2").forEach((image_2) => {
+  image_2.addEventListener("click", () => {
+    var src = image_2.getAttribute("src");
+    document.querySelector('.big-image-2').src = src;
+  });
+});
